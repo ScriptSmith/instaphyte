9dd9711b7f28aae9ab41fa1031f9888fdf9bba4a
ScriptSmith <ScriptSmith@users.noreply.github.com>
2019-02-09 11:22:24 +1000

    bump version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ requirements = [
 ]
 
 setup(name='instaphyte',
-      version='1.0.0',
+      version='1.0.1',
       description='Simple Instagram hashtag and location scraper',
       author='Adam Smith',
       author_email='adam.smith1@uq.edu.au',
